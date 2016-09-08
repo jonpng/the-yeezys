9da868b2fc9a1078d6158d027f4b7702ba72986e
taylorhavrilak <thavrilak3@gatech.edu>
2016-09-08 18:38:50 -0400

    Taylor H edits -> person 4

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -31,7 +31,14 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+      for (int i = 0; i < input.length(); i++) {
+          char c = input.charAt(i);
+          int asciiVal = (int) c;
+          int newAsciiVal = asciiVal + 1;
+
+          String updatedString = updatedString + (char)newAsciiVal + "";
+      }
+      return updatedString;
     }
     
     /**
