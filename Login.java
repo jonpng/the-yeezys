ee0add81d38c40b5e86da2a55487e0a8614fe4b9
Brandon Ah Chong <Brandon@lawn-128-61-73-80.lawn.gatech.edu>
2016-09-20 18:00:16 -0400

    Window Opens when Run

--- Login.java
+++ Login.java
@@ -1,11 +1,16 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.beans.property.StringProperty;
 
 public class Login extends Application {
 
 	public void start(Stage stage) {
-
+		GridPane grid = new GridPane();
+		Scene scene = new Scene(grid);
+		stage.setScene(scene);
+		stage.setTitle("Clean Water Conservation Report");
+		stage.show();
 	}
 }
