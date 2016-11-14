44963c7c27432b19fb6a19cf321aa798f96f7cd3
jvnduong <jvnduong@gmail.com>
2016-11-14 11:54:24 -0500

    PurityReport JUnit

--- M4/test/Junit_Test.java
+++ M4/test/Junit_Test.java
@@ -5,6 +5,9 @@ package test;
 
 import model.Report;
 import model.User;
+import model.PurityReport;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +20,15 @@ public class Junit_Test {
     Report report1 = new Report(0, 0, "Test1", "Waste", "username", "N", "E", "Well");
     Report report2 = new Report(38, 24, "Test2", "Potable", "manager", "S", "E", "Lake");
 
+    Date date1 = new Date();
+    long i = 1479135572;
+    Date date2 = new Date(i);
+    PurityReport pure1 = new PurityReport(0, 0, "Name1", "Fair", "Reporter1", "N", "E", 10, 20, date1);
+    PurityReport pure2 = new PurityReport(52, 48, "Name2", "Good", "Reporter2", "S", "W", 0, 0, date2);
+    String dateStr1 = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(date1);
+    String dateStr2 = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(date2);
+
+
     @Before
     public void setUp() {
         user1 =new User("Name1", "User1", "Pass1".hashCode(), "Worker");
@@ -24,6 +36,11 @@ public class Junit_Test {
 
         report1 = new Report(0, 0, "Test1", "Waste", "username", "N", "E", "Well");
         report2 = new Report(38, 24, "Test2", "Potable", "manager", "S", "E", "Lake");
+
+        pure1 = new PurityReport(0, 0, "Name1", "Fair", "Reporter1", "N", "E", 10.0, 20.0, date1);
+        pure2 = new PurityReport(52, 48, "Name2", "Good", "Reporter2", "S", "W", 0.1, 0.1, date2);
+        dateStr1 = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(date1);
+        dateStr2 = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(date2);
     }
 
     @Test
@@ -47,6 +64,17 @@ public class Junit_Test {
         Assert.assertEquals(report2.getType(), "Lake");
         Assert.assertEquals(report2.getReportName(), "Test2");
         Assert.assertEquals(report2.getReporter(), "manager");
+    }
+
+    @Test
+    public void testPurity() {
+        Assert.assertEquals(pure1.getVirusPPM(), 10, 0.001);
+        Assert.assertEquals(pure1.getContainmentPPM(), 20, 0.001);
+        Assert.assertEquals(pure1.getDate(), dateStr1);
+
+        Assert.assertEquals(pure2.getVirusPPM(), 0.1, 0.001);
+        Assert.assertEquals(pure2.getContainmentPPM(), 0.1, 0.001);
+        Assert.assertEquals(pure2.getDate(), dateStr2);
     }
 
 }
